ccd2deb257b4290ec4ff6e6c9f5817a1e0635e21
TerezaTrckova <146125660+TerezaTrckova@users.noreply.github.com>
2023-12-30 20:16:33 +0100

    Update Q5.sql

--- Q5.sql
+++ Q5.sql
@@ -5,28 +5,28 @@
 -- pomocný view, připojení secondary tabulky samu na sebe, zisk meziročního růstu HDP v %
 CREATE OR REPLACE VIEW v_Q5 AS
 SELECT 
-tttpssf.`year` AS previous_year, 
-tttpssf1.`year`AS current_year,
-tttpssf.GDP AS previous_GDP,
-tttpssf1.GDP current_GDP,
-concat(tttpssf.YEAR, '-', tttpssf1.YEAR) AS time_range,
-round ((tttpssf1.GDP-tttpssf.GDP)/tttpssf.GDP*100,2) AS GDP_change
+	tttpssf.`year` AS previous_year, 
+	tttpssf1.`year`AS current_year,
+	tttpssf.GDP AS previous_GDP,
+	tttpssf1.GDP current_GDP,
+	concat(tttpssf.YEAR, '-', tttpssf1.YEAR) AS time_range,
+	round ((tttpssf1.GDP-tttpssf.GDP)/tttpssf.GDP*100,2) AS GDP_change
 FROM t_tereza_trckova_project_sql_secondary_final tttpssf 
-JOIN t_tereza_trckova_project_sql_secondary_final tttpssf1
-ON tttpssf.year = tttpssf1.YEAR-1  
-AND tttpssf.country = tttpssf1.country 
-WHERE tttpssf.country = 'Czech Republic';
+	JOIN t_tereza_trckova_project_sql_secondary_final tttpssf1
+	ON tttpssf.year = tttpssf1.YEAR-1  
+	AND tttpssf.country = tttpssf1.country 
+	WHERE tttpssf.country = 'Czech Republic';
 
 SELECT * FROM v_Q5;
 -- spojení dat o meziročním růstu HDP s daty o meziročním růstu mezd a cen potravin, vše v % (join na view4a z minulé otázky 4)
 SELECT 
-vq4a.time_range,
-vq5.GDP_change,
-vq4a.salary_change,
-vq4a.foodprice_change
+	vq4a.time_range,
+	concat(vq5.GDP_change,' %') AS GDP_change,
+	concat(vq4a.salary_change, ' %') AS salary_change,
+	concat (vq4a.foodprice_change, ' %') AS foodprice_change
 FROM v_Q5 vq5
-JOIN v_4Qa vq4a
-ON vq4a.time_range = vq5.time_range;
+	JOIN v_4Qa vq4a
+	ON vq4a.time_range = vq5.time_range;
 
 
 
