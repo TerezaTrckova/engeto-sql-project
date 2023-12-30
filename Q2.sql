892b9a56e840d9a772c2cd6da067ca0fd05c1df8
TerezaTrckova <146125660+TerezaTrckova@users.noreply.github.com>
2023-12-30 18:18:45 +0100

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -2,27 +2,34 @@
 
 -- analýza souhrnně za ČR, koli si lze za průměrnou hrubou mzdu (průměr za všechna odvětví v ČR) lze koupit mléka a chleba
 SELECT 
-`year`, 
-product,
-round (avg(average_salary),0) AS average_salary_CR,
-round (avg(average_salary)/average_price,2)  AS Purchasing_power
+  `year`, 
+  product,
+  average_price,
+  round (avg(average_salary),0) AS average_salary_CR,
+  round (avg(average_salary)/average_price,2)  AS Purchasing_power
 FROM t_tereza_trckova_project_sql_primary_final tttpspf 
 WHERE YEAR IN (2006, 2018)
 AND product in('Mléko polotučné pasterované', 'Chléb konzumní kmínový') 
 GROUP BY `year` , product
-ORDER BY `year` , purchasing_power DESC;
+ORDER BY `year` , purchasing_power DESC
 
 -- analýza dle jednotlivých odvětví, v jakém odvětví si mohou za svou hrubou mzdu koupit nejvíce mléka a chleba
-SELECT year, industry_name, product,
-round(average_salary/average_price,2) AS Purchasing_power, unit
+SELECT year, 
+	industry_name, 
+	product,
+	average_salary ,
+	round(average_salary/average_price,2) AS purchasing_power
 FROM t_tereza_trckova_project_sql_primary_final tttpspf 
 WHERE YEAR IN (2006, 2018)
 AND product = 'Mléko polotučné pasterované'
-ORDER BY YEAR, purchasing_power DESC;
+ORDER BY YEAR, purchasing_power DESC
 
-SELECT year, industry_name, product,
-round(average_salary/average_price,2) AS Purchasing_power, unit
+SELECT year, 
+	industry_name, 
+	product,
+	average_salary ,
+	round(average_salary/average_price,2) AS purchasing_power
 FROM t_tereza_trckova_project_sql_primary_final tttpspf 
 WHERE YEAR IN (2006, 2018)
 AND product = 'Chléb konzumní kmínový'
-ORDER BY YEAR, purchasing_power DESC;
+ORDER BY YEAR, purchasing_power DESC
