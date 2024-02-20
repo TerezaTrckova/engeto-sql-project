499321580475ebb3e4edf32973c44a530001e9f1
TerezaTrckova <146125660+TerezaTrckova@users.noreply.github.com>
2024-02-20 14:07:13 +0100

    Update Q5.sql

--- Q5.sql
+++ Q5.sql
@@ -1,32 +1,32 @@
 -- Má výška HDP vliv na změny ve mzdách a cenách potravin? 
--- Neboli,pokud HDP vzroste výrazněji v jednom roce, 
--- projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+-- Neboli, pokud HDP vzroste výrazněji v jednom roce, 
+-- projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
 
 -- pomocný view, připojení secondary tabulky samu na sebe, zisk meziročního růstu HDP v %
-CREATE OR REPLACE VIEW v_Q5 AS
+CREATE OR REPLACE VIEW rust_HDP AS
 SELECT 
-	tttpssf.`year` AS previous_year, 
-	tttpssf1.`year`AS current_year,
+	tttpssf.year AS previous_year, 
+	tttpssf1.year AS current_year,
 	tttpssf.GDP AS previous_GDP,
 	tttpssf1.GDP current_GDP,
-	concat(tttpssf.YEAR, '-', tttpssf1.YEAR) AS time_range,
-	round ((tttpssf1.GDP-tttpssf.GDP)/tttpssf.GDP*100,2) AS GDP_change
+	CONCAT (tttpssf.year, '-', tttpssf1.year) AS time_range,
+	ROUND ((tttpssf1.GDP-tttpssf.GDP)/tttpssf.GDP*100,2) AS GDP_change
 FROM t_tereza_trckova_project_sql_secondary_final tttpssf 
-	JOIN t_tereza_trckova_project_sql_secondary_final tttpssf1
-	ON tttpssf.year = tttpssf1.YEAR-1  
-	AND tttpssf.country = tttpssf1.country 
-	WHERE tttpssf.country = 'Czech Republic';
+JOIN t_tereza_trckova_project_sql_secondary_final tttpssf1
+ON tttpssf.year = tttpssf1.year-1  
+AND tttpssf.country = tttpssf1.country 
+WHERE tttpssf.country = 'Czech Republic';
 
-SELECT * FROM v_Q5;
--- spojení dat o meziročním růstu HDP s daty o meziročním růstu mezd a cen potravin, vše v % (join na view4a z minulé otázky 4)
+SELECT * FROM rust_HDP;
+-- spojení dat o meziročním růstu HDP s daty o meziročním růstu mezd a cen potravin, vše v % (join na view mezirocni_zmena z minulé otázky 4)
 SELECT 
-	vq4a.time_range,
-	concat(vq5.GDP_change,' %') AS GDP_change,
-	concat(vq4a.salary_change, ' %') AS salary_change,
-	concat (vq4a.foodprice_change, ' %') AS foodprice_change
-FROM v_Q5 vq5
-	JOIN v_4Qa vq4a
-	ON vq4a.time_range = vq5.time_range;
+	mz.time_range,
+	CONCAT (HDP.GDP_change, ' %') AS GDP_change,
+	CONCAT (mz.salary_change, ' %') AS salary_change,
+	CONCAT (mz.foodprice_change, ' %') AS foodprice_change
+FROM rust_HDP HDP
+JOIN mezirocni_zmena mz
+ON mz.time_range = HDP.time_range;
 
 
 
