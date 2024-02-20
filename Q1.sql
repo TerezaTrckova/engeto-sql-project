7de76abd9d311789f9e56e3cba3f204bbc35ce47
TerezaTrckova <146125660+TerezaTrckova@users.noreply.github.com>
2024-02-20 13:40:41 +0100

    Update Q1.sql

--- Q1.sql
+++ Q1.sql
@@ -1,11 +1,11 @@
 -- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
 -- pomocný view, join primary tabulky sama na sebe k porovnání meziročního růstu mezd
-CREATE OR REPLACE VIEW view_Q1a AS 
+CREATE OR REPLACE VIEW mezirocni_zmena_mezd AS 
 SELECT DISTINCT 
-	t.YEAR AS previous_year,
+	t.year AS previous_year,
 	t.average_salary AS previous_salary,
-	t1.YEAR AS current_year,
+	t1.year AS current_year,
 	t1.average_salary AS current_salary,
 	t.industry_name, 
 	ROUND ((t1.average_salary-t.average_salary)/t.average_salary*100,2) AS change_
@@ -13,73 +13,73 @@ FROM t_tereza_trckova_project_SQL_primary_final t
 JOIN t_tereza_trckova_project_SQL_primary_final t1
 ON t.year = t1.year -1
 AND t.industry_name=t1.industry_name
-GROUP BY t.YEAR, t.industry_name
+GROUP BY t.year, t.industry_name
 ORDER BY t.industry_name;
 
-SELECT * FROM view_Q1a;
+SELECT * FROM mezirocni_zmena_mezd;
 -- procentuální růst mezd všech odvětví za celé období 2006-2018
 SELECT  
-	concat (round(avg(change_),2), ' %') AS total_growth,
+	CONCAT (round(avg(change_),2), ' %') AS total_growth,
 	industry_name
-FROM view_Q1a
+FROM mezirocni_zmena_mezd
 GROUP BY industry_name
 ORDER BY total_growth DESC;
 
 -- Odvětví, u kterých se objevil alespoň v jednom roce meziroční pokles mezd 
-CREATE OR REPLACE VIEW view_Q1b AS 
+CREATE OR REPLACE VIEW mezirocni_pokles_mezd AS 
 SELECT DISTINCT 
-	t.YEAR AS previous_year,
+	t.year AS previous_year,
 	t.average_salary AS previous_salary,
-	t1.YEAR AS current_year,
+	t1.year AS current_year,
 	t1.average_salary AS current_salary,
 	t.industry_name, 
 	ROUND ((t1.average_salary-t.average_salary)/t.average_salary*100,2) AS decline,
-	concat(t.YEAR, '-', t1.YEAR) AS time_range
+	CONCAT (t.year, '-', t1.year) AS time_range
 FROM t_tereza_trckova_project_SQL_primary_final t
 JOIN t_tereza_trckova_project_SQL_primary_final t1
 ON t.year = t1.year -1
 AND t.industry_name=t1.industry_name
 WHERE t1.average_salary < t.average_salary
-GROUP BY t.YEAR, t.industry_name
+GROUP BY t.year, t.industry_name
 ORDER BY t.industry_name;
 
 -- Po zaokrouhlení jedna z hodnot proměnné decline vychází 0 ('Doprava a skladování'), nutno ji odfiltrovat 
 -- kdy a jak velký se objevil meziroční pokles mezd u daných odvětví (v %)
-SELECT * FROM view_Q1b;
+SELECT * FROM mezirocni_pokles_mezd;
 
 SELECT 
 	time_range,		
 	concat (decline,' %') AS decline,
 	industry_name
-FROM view_Q1b
+FROM mezirocni_pokles_mezd
 having decline < 0
 ORDER BY decline DESC;
  
 -- Odvětví, u kterých se objevil alespoň v jednom roce meziroční růst mezd 
-CREATE OR REPLACE VIEW view_Q1c AS 
+CREATE OR REPLACE VIEW mezirocni_rust_mezd AS 
 SELECT DISTINCT 
-	t.YEAR AS previous_year,
+	t.year AS previous_year,
 	t.average_salary AS previous_salary,
-	t1.YEAR AS current_year,
+	t1.year AS current_year,
 	t1.average_salary AS current_salary,
 	t.industry_name, 
 	ROUND ((t1.average_salary-t.average_salary)/t.average_salary*100,2) AS growth,
-	concat(t.YEAR, '-', t1.YEAR) AS time_range
+	CONCAT (t.year, '-', t1.year) AS time_range
 FROM t_tereza_trckova_project_SQL_primary_final t
 JOIN t_tereza_trckova_project_SQL_primary_final t1
 ON t.year = t1.year -1
 AND t.industry_name=t1.industry_name
 WHERE t1.average_salary > t.average_salary
-GROUP BY t.YEAR, t.industry_name
+GROUP BY t.year, t.industry_name
 ORDER BY growth DESC;
 
 -- 5 nejvyšších meziročních růstů mzdy napříč odvětvími 
-SELECT * FROM view_q1c;
+SELECT * FROM mezirocni_rust_mezd;
 
 SELECT 
 	time_range,		
-	concat (growth,'%') AS growth,
+	CONCAT (growth,'%') AS growth,
 	industry_name
-FROM view_Q1c
+FROM mezirocni_rust_mezd
 LIMIT 5; 
 
