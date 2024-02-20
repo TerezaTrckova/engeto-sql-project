c23f12f18de710c19c5bbe633d7567b7c40d80cb
TerezaTrckova <146125660+TerezaTrckova@users.noreply.github.com>
2024-02-20 13:48:46 +0100

    Update Q2.sql

--- Q2.sql
+++ Q2.sql
@@ -1,35 +1,37 @@
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
--- analýza souhrnně za ČR, koli si lze za průměrnou hrubou mzdu (průměr za všechna odvětví v ČR) lze koupit mléka a chleba
+-- analýza souhrnně za ČR, kolik si lze za průměrnou hrubou mzdu (průměr za všechna odvětví v ČR) lze koupit mléka a chleba
 SELECT 
-  `year`, 
-  product,
-  average_price,
-  round (avg(average_salary),0) AS average_salary_CR,
-  round (avg(average_salary)/average_price,2)  AS Purchasing_power
+  	year, 
+  	product,
+ 	average_price,
+ 	ROUND (AVG(average_salary),0) AS average_salary_CR,
+  	ROUND (AVG(average_salary)/average_price,2) AS purchasing_power
 FROM t_tereza_trckova_project_sql_primary_final tttpspf 
-WHERE YEAR IN (2006, 2018)
-AND product in('Mléko polotučné pasterované', 'Chléb konzumní kmínový') 
-GROUP BY `year` , product
-ORDER BY `year` , purchasing_power DESC
+WHERE year IN (2006, 2018)
+AND product IN ('Mléko polotučné pasterované', 'Chléb konzumní kmínový') 
+GROUP BY `year`, product
+ORDER BY `year`, purchasing_power DESC
 
 -- analýza dle jednotlivých odvětví, v jakém odvětví si mohou za svou hrubou mzdu koupit nejvíce mléka a chleba
-SELECT year, 
+SELECT 
+	year, 
 	industry_name, 
 	product,
 	average_salary ,
-	round(average_salary/average_price,2) AS purchasing_power
+	ROUND (average_salary/average_price,2) AS purchasing_power
 FROM t_tereza_trckova_project_sql_primary_final tttpspf 
-WHERE YEAR IN (2006, 2018)
+WHERE year IN (2006, 2018)
 AND product = 'Mléko polotučné pasterované'
-ORDER BY YEAR, purchasing_power DESC
+ORDER BY year, purchasing_power DESC
 
-SELECT year, 
+SELECT 
+	year, 
 	industry_name, 
 	product,
 	average_salary ,
-	round(average_salary/average_price,2) AS purchasing_power
+	ROUND (average_salary/average_price,2) AS purchasing_power
 FROM t_tereza_trckova_project_sql_primary_final tttpspf 
-WHERE YEAR IN (2006, 2018)
+WHERE year IN (2006, 2018)
 AND product = 'Chléb konzumní kmínový'
-ORDER BY YEAR, purchasing_power DESC
+ORDER BY year, purchasing_power DESC
